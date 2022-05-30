6a4ac0db4ffdf4bdcf3ff6557f2f01cd8f6a85ce
@NDE18 <rodriguekongne@gmail.com>
2022-05-30 02:48:58 +0200

    db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,22 @@
-module.exports = {
+/*module.exports = {
   HOST: "localhost",
   USER: "root",
   PASSWORD: "",
   DB: "pv_db",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};*/
+
+module.exports = {
+  HOST: "localhost",
+  USER: "68.66.248.39:2082",
+  PASSWORD: "O]y_$01_MnCB",
+  DB: "epanzyco_stat_db",
   dialect: "mysql",
   pool: {
     max: 5,
